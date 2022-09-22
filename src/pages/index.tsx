af1306f4a3286d459ff870b4847a6732e706a6fd
Kingdom Desires <KingdomDesires@gmail.com>
2022-09-22 18:55:49 -0500

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,111 @@
-import type { NextPage } from 'next'
-import React, { useEffect, useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '$styles/Home.module.css'
+import type { NextPage } from 'next';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '$styles/Home.module.css';
 // REPorts
-import Header from '$Sections/Header'
-import Dash from '$Sections/Dash'
-import Testy from '$Sections/Testy'
-import Nav from '$Sections/Nav'
-import Panel from '$Sections/Panel'
-import Auth from '$components/Auth'
-import styled from 'styled-components'
-import { supabase } from '../../client.js'
-import { useRouter } from 'next/router'
+import Header from 'src/#Sections^/Header';
+import Dash from 'src/#Sections^/Dash';
+import Testy from 'src/#Sections^/Testy';
+import Nav from 'src/#Sections^/Nav';
+import Panel from 'src/#Sections^/Panel';
+import Auth from 'src/@components^/Auth';
+import styled from 'styled-components';
+import { supabase } from '../../client.js';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [session, setSession] = useState<any>(null)
-  const router = useRouter()
+    const [isLoading, setIsLoading] = useState(true);
+    const [session, setSession] = useState<any>(null);
+    const router = useRouter();
 
+    useEffect(() => {
+        let mounted = true;
 
-  useEffect(() => {
-    let mounted = true
+        async function getInitialSession() {
+            const {
+                data: { session },
+            } = await supabase.auth.getSession();
+            console.log(`session`);
+            console.log(session);
+            // only update the react state if the component is still mounted
+            if (mounted) {
+                if (session) {
+                    setSession(session);
+                }
 
-    async function getInitialSession() {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession()
-      console.log(`session`)
-      console.log(session)
-      // only update the react state if the component is still mounted
-      if (mounted) {
-        if (session) {
-          setSession(session)
+                setIsLoading(false);
+            }
         }
 
-        setIsLoading(false)
-      }
-    }
+        getInitialSession();
 
-    getInitialSession()
+        // const { subscription } = supabase.auth.onAuthStateChange(
+        //   (_event, session) => {
+        //     setSession(session)
+        //   }
+        // )
 
-    // const { subscription } = supabase.auth.onAuthStateChange(
-    //   (_event, session) => {
-    //     setSession(session)
-    //   }
-    // )
+        return () => {
+            mounted = false;
 
-    return () => {
-      mounted = false
+            // subscription?.unsubscribe()
+        };
+    }, []);
 
-      // subscription?.unsubscribe()
-    }
-  }, [])
+    useEffect(() => {
+        if (session) {
+            router.push('/Home');
+        }
+        if (!session) {
+            router.push('/Home');
+        }
+    }, [session]);
 
-  useEffect(() => {
-    if (session) {
-      router.push('/Home')
-    }
-    if (!session) {
-      router.push('/Home')
-    }
-  }, [session])
-
-  return (
-        <Auth />
-  )
+    return <Auth />;
 }
 
-
 const WebFire_App = styled.div`
-  border: 2px solid white;
-  height: 100%;
-  width: 100%;
-  display: block;
-`
+    border: 2px solid white;
+    height: 100%;
+    width: 100%;
+    display: block;
+`;
 
 const WebFire_App_Shell_Parent = styled.div`
-  display: flex;
-  height: 100%;
-  width: 100%;
-  background-color: #1a1b1c;
-`
+    display: flex;
+    height: 100%;
+    width: 100%;
+    background-color: #1a1b1c;
+`;
 
 const WebFire_App_Shell = styled.div`
-  display: flex;
-  flex: 1 1 auto;
-  flex-direction: column;
-  height: 100%;
-  overflow: hidden;
-  position: relative;
-  width: 100%;
-`
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+`;
 
 const WebFire_App_Shell_Content = styled.div`
-  display: flex;
-  flex: 1 1 auto;
-  overflow: hidden;
-  position: relative;
-  border: 2px solid white;
-`
+    display: flex;
+    flex: 1 1 auto;
+    overflow: hidden;
+    position: relative;
+    border: 2px solid white;
+`;
 
 const MainFlexContainer = styled.main`
-  display: flex;
-  /* flex: 1 1 auto; */
-  width: calc(100% - 166px);
-  left: 166px;
-  overflow: hidden;
-  left: 166px;
-  position: relative;
-  border: 4px solid yellow;
-  border-top-left-radius: 12px;
-`
+    display: flex;
+    /* flex: 1 1 auto; */
+    width: calc(100% - 166px);
+    left: 166px;
+    overflow: hidden;
+    left: 166px;
+    position: relative;
+    border: 4px solid yellow;
+    border-top-left-radius: 12px;
+`;
